ac3bcd0ffbc969917e6db7a90160d6ce04327a50
nakano <aiueo.candy3726@gmail.com>
2020-03-22 09:28:51 +0000

    ログイン、新規登録外観

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
   VALID_PASSWORED_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
-  validates :description, length: {maximum: 300 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :description, length: { maximum: 300 }
+  validates :password_digest, length: { maximum: 6 }
   
   has_secure_password
 end
